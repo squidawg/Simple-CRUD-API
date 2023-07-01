0a19a8eaf4932073d03edabbf77a7253966ca51f
squidawg <78506807+squidawg@users.noreply.github.com>
2023-07-01 14:24:22 +0400

    fix: refactor id for tests

--- src/data/users.ts
+++ src/data/users.ts
@@ -1,15 +1,16 @@
 import {User} from "../models/user.model";
 
-export const userDara: User[] = [
+export const userRecords: User[] = [
     {
+        id: 'e561cbef-687a-489c-8b3e-4a15e54ff2c1',
         username: 'Alex',
         age: 32,
         hobbies: ['drinking']
     },
     {
+        id: '9650e277-1bc6-41cc-a6c2-b5d4292e9128',
         username: 'Rachel',
         age: 4,
         hobbies: ['sleeping']
     }
 ]
-
