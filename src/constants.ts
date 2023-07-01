fd1d1456fd32b0983888f197c273e7495cfbcf28
squidawg <78506807+squidawg@users.noreply.github.com>
2023-07-01 14:22:29 +0400

    fix: update constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,13 @@
 export const PORT = process.env.PORT || 3000;
 export const ENDPOINT = '/api/users';
+
+export enum HTTP_METHOD {
+    GET ='GET',
+    POST ='POST',
+    PUT ='PUT',
+    DELETE ='DELETE'
+}
+
+export enum HTTP_STATUS_CODE {
+    OK = 200
+}
