a65303b46f4f19a1676395f0f8aeea257bada61c
squidawg <78506807+squidawg@users.noreply.github.com>
2023-07-02 12:48:35 +0400

    feat: add error handling for index

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
-import {PORT} from './constants';
+import {HTTP_STATUS_CODE, PORT} from './constants';
 import * as http from "http";
 import * as userController from "./controllers/controller";
 import {onRequestType} from "./utils/utils";
+import {responseHandler} from "./controllers/dataHandler";
 
 const server = http.createServer(async (req: http.IncomingMessage, res:http.ServerResponse) => {
     await onListenRequest(req,res)
@@ -15,17 +16,25 @@ process.on('SIGINT',  () => {
 
 
 export const onListenRequest = async (req: http.IncomingMessage, res:http.ServerResponse) => {
-  const request = onRequestType(req);
-    switch (request) {
-        case "getUsers": await userController.onGetUsers(res);
-            break;
-        case "getUser": await userController.onGetUser(req, res);
-            break;
-        case "postUser": await userController.onPostUser(req, res);
-            break;
-        case "putUser": await userController.onPutUser(req, res);
-            break;
-        case "deleteUser": await userController.onDeleteUser(req, res);
-            break;
-    }
+  try{
+      const defMsg = 'non-existing endpoint'
+      const request = onRequestType(req);
+      switch (request) {
+          case "getUsers": await userController.onGetUsers(res);
+              break;
+          case "getUser": await userController.onGetUser(req, res);
+              break;
+          case "postUser": await userController.onPostUser(req, res);
+              break;
+          case "putUser": await userController.onPutUser(req, res);
+              break;
+          case "deleteUser": await userController.onDeleteUser(req, res);
+              break;
+          default: responseHandler(res, HTTP_STATUS_CODE.NOT_FOUND, defMsg)
+      }
+  }
+  catch (e) {
+      const errorMessage = 'Error on the server side'
+      responseHandler(res, HTTP_STATUS_CODE.ERR_SERVER_SIDE, errorMessage)
+  }
 }
